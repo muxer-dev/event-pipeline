d9838d9a281d820aae3a4afb54191b726d65a1ac
Kyle Harrison <kyle90adam@hotmail.com>
2019-02-12 21:21:40 +0000

    Adding structured json logging

---
+++ lib/common/exceptions.py
@@ -0,0 +1,2 @@
+class InvalidLogLevelName(Exception):
+    pass
---
+++ lib/load.py
@@ -0,0 +1,11 @@
+from common.logger import logger
+
+
+def handle(event, context):
+    logger.info(f"load: {event}")
+
+    body = {"message": "Your load function executed successfully!", "data": event}
+
+    response = {"statusCode": 200, "body": body}
+
+    return response
