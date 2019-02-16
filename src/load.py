1e820afdfe4a59bddc9dd847fa39cb60c4b0df7e
Kyle Harrison <kyle90adam@hotmail.com>
2019-02-16 20:27:07 +0000

    Adding load function

--- src/load.py
+++ src/load.py
@@ -1,11 +1,42 @@
+import json
+import os
+
+import requests
+from jsonpointer import resolve_pointer as resolve
+
 from src.common.logger import logger
+
+EVENTS_ENDPOINT = os.getenv("EVENTS_ENDPOINT")
+
+
+def _post_payloads(payloads):
+    success = 0
+    failures = 0
+
+    for payload in payloads:
+        r = requests.post(
+            EVENTS_ENDPOINT,
+            headers={"Content-type": "application/json"},
+            data=json.dumps(payload),
+        )
+        logger.info(f"post: {r.status_code}")
+
+        if r.status_code == requests.codes.ok:
+            success += 1
+        else:
+            failures += 1
+
+    return {"success": success, "failure": failures}
 
 
 def handle(event, context):
     logger.info(f"load: {event}")
 
-    body = {"message": "Your load function executed successfully!", "data": event}
+    sources = resolve(event, "/sources", [])
 
-    response = {"statusCode": 200, "body": body}
+    responses = []
+    for source in sources:
+        events = resolve(source, "/events", [])
+        responses.append(_post_payloads(events))
 
-    return response
+    return {"responses": responses}
